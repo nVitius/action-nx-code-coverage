090ba0371e36666ddc72394da5e76f67ee0ec4db
Mauricio <mauricio@nviti.us>
2023-05-03 16:42:11 -0700

    fix(badge): Skip gist update if token/id is missing

--- src/main.ts
+++ src/main.ts
@@ -82,7 +82,9 @@ export const main = async ({
 
       const files = buildGistCoverageFileList(results)
 
-      updateCoverageGist({files, gistToken, gistId})
+      if (gistToken && gistId) {
+        updateCoverageGist({files, gistToken, gistId})
+      }
     }
 
     return results
